03e484878dbba74d9fbf67e5a55dbbc5d1947e75
Mateus Garcia <mateus.gigainfo@gmail.com>
2023-05-25 10:48:09 -0300

    add swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags( 'Textract OCR' )
 @Controller()
 export class AppController {
   constructor( private readonly appService: AppService ) { }
 
-  @Get()
+
+  @ApiOperation( {
+    summary: 'Envia o documento para a AWS e retorna a análise dos campos detectados no ocr.'
+  } )
+  @Get( 'poc' )
   poc () {
     return this.appService.poc();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { config } from 'dotenv'; config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap () {
   const app = await NestFactory.create( AppModule );
+  const options = new DocumentBuilder();
+  options.setTitle( 'PoC OCR chave-valor com AWS Textract' )
+  options.setDescription( 'extraindo chave/valor de documentos através do OCR do AWS Textract' )
+  const document = SwaggerModule.createDocument( app, options.build() );
+  SwaggerModule.setup( '', app, document );
   await app.listen( 3000 );
 }
 bootstrap();
